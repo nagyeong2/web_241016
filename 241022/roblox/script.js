03ac3118297af3c5991b6ac84bacb41dcf32eab8
nagyeong2you@example.com <skrldpdy@naver.com>
2024-10-29 22:01:41 +0900

    241029

--- 241022/roblox/script.js
+++ 241022/roblox/script.js
@@ -1,5 +1,13 @@
-const femalebtn = document.getElementById("femailebtn");
-femalebtn.addEventListener("click", (e) => {
+const femaleBtn = document.getElementById("femalebtn");
+femaleBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("나 여기있어요!");
+  document.querySelector("#femalebtn i").classList.add("filledA");
+  document.querySelector("#malebtn i").classList.remove("filledB");
+});
+
+const maleBtn = document.getElementById("malebtn");
+maleBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("#malebtn i").classList.add("filledB");
+  document.querySelector("#femalebtn i").classList.remove("filledA");
 });
