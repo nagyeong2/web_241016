941e62e8bc63a99b19c4d7583afa739ba1813120
nagyeong2you@example.com <skrldpdy@naver.com>
2024-11-01 21:48:41 +0900

    241101

--- 241022/roblox/script.js
+++ 241022/roblox/script.js
@@ -1,3 +1,4 @@
+/*button event*/
 const femaleBtn = document.getElementById("femalebtn");
 femaleBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -10,4 +11,19 @@ maleBtn.addEventListener("click", (e) => {
   e.preventDefault();
   document.querySelector("#malebtn i").classList.add("filledB");
   document.querySelector("#femalebtn i").classList.remove("filledA");
+});
+
+/*logon & login event*/
+const userInput = document.querySelector(".userInput");
+const formElement = document.querySelector("form");
+
+console.log(formElement);
+
+formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // const userName = userInput.value;
+  // console.log(userName);
+  alert(`${document.querySelector(".userInput").value}님! 아직 준비중입니다!`);
+  document.querySelector(".userInput").value = "";
+  document.querySelector(".PasswordInput").value = "";
 });
